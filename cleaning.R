216195aeec7a4f7315dff56cc27eb6266529f190
Max Zhang <max.zhang@berkeley.edu>
2021-11-25 13:14:46 -0800

    create cleaning script

---
+++ cleaning.R
@@ -0,0 +1,31 @@
+
+library(tidyverse)
+library(sjlabelled)
+
+load('ICPSR_20520/DS0001/20520-0001-Data.rda')
+
+cils <- as_tibble(da20520.0001)
+
+labels <- attributes(cils)$variable.labels
+
+cils <- remove_all_labels(cils)
+
+numeric <- cils %>% 
+  select(!where(is.factor))
+
+nonnumeric <- cils %>% 
+  select(where(is.factor))
+
+test <- nonnumeric %>% 
+  apply(MARGIN = 2, FUN = function(x) length(unique(x)))
+
+unordered <- nonnumeric[test > 6]
+ordered <- nonnumeric[test <= 6]
+
+
+
+
+
+
+
+
