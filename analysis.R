eb8f538a1079878a6346ef4024e8bde502304874
Max Zhang <myzhang01@berkeley.edu>
2021-12-11 13:12:58 -0800

    reorganize code

--- analysis.R
+++ analysis.R
@@ -1,34 +1,40 @@
 
 library(glmnet)
-library(lars)
 
-#source('cleaning.R')
+# combine
+combined <- cbind(income, numeric, categorical)
 
-# do lasso's
-lasso <- cv.glmnet(x = train,
-                   y = income_train,
-                   type.measure = 'mse',
-                   standardize = TRUE,
-                   family = 'gaussian',
-                   alpha = 1)
+# test-train split
+set.seed(123)
 
+indices <- sample(1:nrow(combined), size = 0.7*nrow(combined))
+
+test <- combined[-indices, ]
+train <- combined[indices, ]
+
+# LASSO
+cv <- cv.glmnet(x = train[ ,-1],
+                y = train[ ,1],
+                type.measure = 'mse',
+                standardize = TRUE,
+                family = 'gaussian',
+                alpha = 1)
 
 # get names of nonzero coefficient variables
 # max stole all this code so don't ask him how it works
-tmp_coeffs <- coef(lasso, s = "lambda.min")
+tmp_coeffs <- coef(cv, s = "lambda.min")
 tmp_coeffs <- data.frame(name = tmp_coeffs@Dimnames[[1]][tmp_coeffs@i + 1], coefficient = tmp_coeffs@x)
 tmp_coeffs <- tmp_coeffs[-1, ]
 
-
 # create formula for lm
 coeffs <- tmp_coeffs$name %>%
-  str_replace_all('[[:punct:]]*[[:space:]]*', '')
-formula <- paste('income_test ~', paste(coeffs, collapse = ' + '))
+  str_replace_all('[[:punct:]]*[[:space:]]*[=]*', '')
+formula <- paste('income ~', paste(coeffs, collapse = ' + '))
 
-colnames(test) <- str_replace_all(colnames(test), '[[:punct:]]*[[:space:]]*', '')
+colnames(test) <- str_replace_all(colnames(test), '[[:punct:]]*[[:space:]]*[=]*', '')
 
 reg <- lm(formula = formula,
-          data = cbind(as.data.frame(test), income_test))
+          data = as.data.frame(test))
 summary(reg)
 
 
---
+++ cleaning-old.R
@@ -0,0 +1,188 @@
+
+library(tidyverse)
+library(sjlabelled)  # for remove_all_labels()
+
+
+# Import ------------------------------------------------------------------
+
+load('ICPSR_20520/DS0001/20520-0001-Data.rda')
+
+labels <- attributes(da20520.0001)$variable.labels
+
+cils <- as_tibble(da20520.0001) %>%
+  filter(!is.na(V421)) %>%  # remove all observations with missing income
+  remove_all_labels() %>%
+  select(!matches('V4[[:alnum:]]{2,}'))  # remove 2005 variables
+cils_inc_missing <- as_tibble(da20520.0001) %>%
+  filter(is.na(V421))
+
+
+# Deal with numeric variables ---------------------------------------------
+
+numeric <- cils %>%
+  select(where(is.double)) %>%
+  select(!CASEID)
+
+numeric_na <- numeric %>%
+  lapply(FUN = function(x) sum(is.na(x)) / length(x)) %>%
+  unlist()
+numeric_na <- names(numeric_na[numeric_na >= 0.5])  # the 0.5 is the threshold for removing NAs
+
+numeric <- numeric %>%
+  select(!all_of(numeric_na)) %>%
+  mutate(across(.cols = everything(), is.na, .names = '{.col}_NA')) %>%
+  mutate(across(.cols = ends_with('_NA'), as.double)) %>%
+  # mean imputation: for all columns not ending with NA, replace missing value
+  # with the mean of the column
+  mutate(across(.cols = !ends_with('_NA'), ~if_else(is.na(.), mean(., na.rm = TRUE), .)))
+numeric <- as.matrix(numeric)
+
+# Deal with non-numeric variables -----------------------------------------
+
+nonnumeric <- cils %>%
+  select(where(is.factor))
+
+nonnumeric_na <- nonnumeric %>%
+  lapply(FUN = function(x) sum(is.na(x)) / length(x)) %>%
+  unlist()
+nonnumeric_na <- names(nonnumeric_na[nonnumeric_na >= 0.5])
+
+nonnumeric <- nonnumeric %>%
+  select(!all_of(c(nonnumeric_na, 'V32', 'V37', 'V233', 'V238', 'V263', 'V62', 'V64', 'V264'))) %>%
+  mutate(across(.cols = everything(), addNA))
+
+nonnumeric_mat <- model.matrix(~., data = nonnumeric)[ ,-1]
+
+
+# Test-train split --------------------------------------------------------
+
+combined <- cbind(numeric, nonnumeric_mat)
+
+set.seed(123)
+indices <- sample(1:nrow(combined), size = 0.7*nrow(combined))
+
+income <- as_tibble(da20520.0001) %>%
+  filter(!is.na(V421)) %>%
+  pull(V421) %>%
+  log()
+income_train <- income[indices]
+income_test <- income[-indices]
+
+train <- combined[indices, ]
+test <- combined[-indices, ]
+
+
+
+
+
+# older code below --------------------------------------------------------
+
+unordered <- nonnumeric %>%
+  select(where(function(x) nlevels(x) > 6))  # does not count NA as a level,
+                                             # therefore only 90 vars selected
+                                             # instead of 98 in previous code
+ordered <- nonnumeric %>%
+  select(where(function(x) nlevels(x) <= 6))
+
+#ordered_num <- ordered %>%
+#  lapply(FUN = as.integer) %>%
+#  lapply(FUN = function(x) (x - mean(x, na.rm = TRUE)) / sd(x, na.rm = TRUE))
+
+
+# Removing mostly NA columns ----------------------------------------------
+
+na_proportions <- da20520.0001 %>%
+  #summarize(across(.fns = function(x) sum(is.na(x)) / length(x)))
+  lapply(FUN = function(x) sum(is.na(x)) / length(x))
+names(na_proportions) <- labels
+
+# Fix unordered and ordered -----------------------------------------------
+
+#Need Debate on V5, V7, V18, V42, V206, P67, P74C, P76, C8, C9, V430 series
+#Some of the column at the end repeat
+false_ordered <- ordered %>%
+  select(V2,    V5,    V7,    V18,   V34A,
+         V39A,  V42,   V88,   V89,   V90,
+         V97,   V111A, V111B, V112A, V112B,
+         V113A, V113B, V147,  V206,  V237,
+         V242,  V297,  P2A,   P4,    P12,
+         P13,   P14,   P15,   P37,   P38,
+         P40,   P42,   P45B,  P50,   P59A,
+         P61,   P74A,  P74C,  P76,   P82,
+         P140,  P142,  C1,    C8,    C9,
+         C10,   C11,   C12,   C13)
+#V416, V417, V418, V425, V430B, V430C, V430D,
+#V430E, V431, V432, V436, V438, V441, V447,
+#V443, V442
+
+#need Debate on V4, V205, V414B, V415B, V440
+false_unordered <- unordered %>%
+  select(V29A, V29C, V29D, V29E, V30,
+         V31,  V205, V236, V241, P56,  # V94, V294, V296 correctly caught by line 24
+         P134)
+#V414B, V415B, V422, V440, V446
+
+
+# Dealing with unordered variables ----------------------------------------
+
+# relabel unordered variables
+labels_unordered <- labels[names(unordered)]
+attr(unordered, "variable.labels") <- labels_unordered
+
+# Consider converting the following columns to dummies:
+dummies <- unordered %>%
+  select(V34B, V46, V228, V266, V295, V323, P3, P30, P53A, P65, P81)
+labels[names(dummies)]  # print dummy variable names
+
+# Consider converting the following columns to dummies as a group:
+dummies_language <- unordered %>%
+  select(V50, V56, V58, V59, V76, V250, V256, V258, V259, V276, P26A, P26B,
+         P27A, P26B, P27A, P27B)
+labels[names(dummies_language)]
+
+dummies_reason_come_to_US <- unordered %>%
+  select(V66, V69)
+labels[names(dummies_reason_come_to_US)]
+
+# Consider converting them to binary variables.
+binary <- unordered %>%
+  select(V87, V246, V287, P34, P48, P68A) # P70, P71 never selected, they have 6 levels
+labels[names(binary)]
+
+# Consider converting them to ordered variables.
+false_unordered_sam <- unordered %>%
+  select(V29A, V29C, V29D, V29E, V30, V31, V236,  # V94, V96, V294, V296 already correctly caught
+         P17, P31, P47, P56, P132, P134)     # P72 never selected, 6 levels
+labels[names(false_unordered_sam)]
+
+# Substituting occupations using numeric prestige score:
+occupations_to_scores <- unordered %>%
+  select(V32, V37, V233, V238, V263, V62, V64, V264)
+labels[names(occupations_to_scores)]
+
+# Substituting using continents or using V278:
+countries_to_continents <- unordered %>%
+  select(V9 ,V15 ,V21 ,V21A , P6, P7 ,P16, P19, P33A)
+labels[names(countries_to_continents)]
+
+# Other Substitutions
+select_two <- unordered %>%
+  select(P35, P36, P49, P51, V323A, V323)
+labels[names(select_two)]
+
+use_constructed_variables <- unordered %>%
+  select(V50, V56, V58, V59)
+labels[names(use_constructed_variables)]
+
+# Consider dropping:
+not_informative <- unordered %>%
+  select(V4,V204,V205,P51)
+labels[names(not_informative)]
+
+mostly_NA <- unordered %>%
+  select(P39A, P41A, P43A, P45A, P46, P53B, P53C, P83, P85)
+labels[names(mostly_NA)]
+
+# Discuss: V28,
+# (V8, V14 > dummies)
+
